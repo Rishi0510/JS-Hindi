fb1a8ffb5fb1c7847c2066627164e73633aaaea3
Rishi Raj <103258238+Rishi0510@users.noreply.github.com>
2024-01-07 18:27:03 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // Treat all JS code as newer version
+//  alert(3); // we are using node js and not browser so it wont work here
+
+let name = "hitesh"
+let age = 19
+let isLoggedIn = true
+
+// Number -> 2^53 is the range
+// bigint for bigger numbers
+// string = ""
+// boolean = true / false
+// Null -> standalone value, represantaion of empty value, let state = null
+// Undefined -> eg let state; idhar undefined hai, value not assigned
+// symbol (used alot in react to find uniqueness)
+
+// object
+
+console.log(typeof null) // NULL IS OBJECT
+console.log(typeof undefined) // UNDEF IS UNDEF
