a3596d98d21f12010594ae4a88f5db0eb46b1ee3
Rishi Raj <103258238+Rishi0510@users.noreply.github.com>
2024-01-08 17:15:23 +0000

    03 completed

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -26,6 +26,34 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 // generally browser give you strings
+
+// ********* OPERATIONS ********* 
+
+let str1 = "hello"
+let str2 = " Rishi"
+
+let str3 = str1 + str2
+// console.log(str1 + str2)
+
+// console.log("1" + 2) // = 12
+// console.log(1 + "2") // = 12
+// console.log("1" + 2 + 2) // = 122 and not 14
+// BUT
+// console.log(1 + 2 + "2") // = 32 and not 122
+
+// whichever is first, gets convertef to it
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3 NOT READABLE
+// num1 = num2 = num3 = 2+2
+// console.table([num1,num2,num3])
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// prefix and postfil js mdn
