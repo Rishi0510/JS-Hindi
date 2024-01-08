aaab1028dd6c9af5124cd7b2e2549798de369305
Rishi Raj <103258238+Rishi0510@users.noreply.github.com>
2024-01-08 18:51:34 +0000

    comprasions done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// node 01_basics/04_comparision.js
+// 2>1 to true hi dega 
+
+// console.log("0" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >=0 )
+
+// Comparison and equality checks work differently
+// in JS, >= & > works differently. when equality, it convets it to number and then compares so null converted to 0 the null (0) >= 0 true
+// console.log(undefined == 0) // (in undefined all 3 false)
+
+// STRICT CHECK === checks VALUE + DATA TYPE
+// console.log("2" == 2) // ye "2" => 2 me convert kardega
+// console.log("2" === 2)
