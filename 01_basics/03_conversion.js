b53172d708a750b4dde5f209dbbad76bb2168edd
Rishi Raj <103258238+Rishi0510@users.noreply.github.com>
2024-01-08 16:44:46 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,31 @@
+// node 01_basics/03_conversion.js
+// let score = null
+
+// console.log(typeof score) 
+// console.log(typeof(score)) // as a method
+
+// let valueInNumber = Number(score) // converting value to number using Number()
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//  TO number
+// "33" -> 33
+// "33abc" -> NaN NotANumber typeof number
+// true -> 1
+// undefined -> undefined
+// null -> 0
+
+// let isLoggedIn = "rishi"
+//  To BOOLEAN
+//  0 -> false, 1 -> true
+//  "" -> false, "rishi" -> true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+// generally browser give you strings
