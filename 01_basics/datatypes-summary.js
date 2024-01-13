3b99e1f20a1fd1c0f5912b983afc8bef039e2a1f
Rishi Raj <103258238+Rishi0510@users.noreply.github.com>
2024-01-13 05:33:12 +0000

    Stack and Heap modified

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -20,7 +20,7 @@ const id1 = Symbol(2014)
 // console.log(typeof(id))
 const id2 = Symbol(2014)
 
-console.log(id1 === id2)
+// console.log(id1 === id2)
 // two symbols with same values still will be different which serves the purpose of a Symbol. Making things unique.
 
 // bigint - Last me n lagado
@@ -39,9 +39,37 @@ let myObj =
 
 // Functions ko bhi variable ki tarah treat kar sakte hain js me
 
-// function(){} ab isok store krna hai
-const myFunction = function(){
-    console.log("Hello");
-}
-console.log(typeof myFunction) 
+// function(){} ab isko store krna hai
+// const myFunction = function(){
+    // console.log("Hello");
+// }
+// console.log(typeof myFunction) 
 // = function or function Object
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// STACK & HEAP(non primitive)
+// stack ke andar define hota hai to copy milta hia, heap me reference
+
+let myYoutubeName = "Rishi"
+let anotherName = "myYoutubeName"
+anotherName = "RishiAurCode"
+
+// console.log(myYoutubeName)
+// console.log(anotherName)
+
+let userOne = {
+    email:"user@google.com",
+    upi: "user@paytm"
+}
+
+let userTwo = userOne
+userTwo.email = "userTwo@google.com"
+
+// yahan userTwo ko direct reference mila heap se , userOne ka islie jab userTwo change kiye to wo memory me change kardiya..
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
