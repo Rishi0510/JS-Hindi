6793a50f557a4258bafbb8d6eb0621d5af3f8635
Rishi Raj <103258238+Rishi0510@users.noreply.github.com>
2024-01-13 13:29:57 +0000

    Math&Num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,20 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// C++ int a = new int(10)
+// console.log(balance)
+// this is now object, so lots of methods
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// PRECISIION LEFT
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+// commas representation ..
+// console.log(Number.MIN_VALUE)
+
+// +++++++++++++ MATHS +++++++++++++
+
+console.log(Math);
